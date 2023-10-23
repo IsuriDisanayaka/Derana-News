504233a5a73da5c19260998f004e937e9ee6ed46
IsuriDisanayaka <isuriumeshika1@gmail.com>
2023-10-23 16:04:46 +0530

    #3 Feat : admin login backend

--- news_api/api/db/server.js
+++ news_api/api/db/server.js
@@ -3,11 +3,14 @@ const cors = require('cors');
 const connectDB = require('./db');
 const express = require('express');
 
+const adminRoutes = require('../routes/Admin')
+
 const PORT = process.env.PORT
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(adminRoutes);
 
 
 connectDB();
